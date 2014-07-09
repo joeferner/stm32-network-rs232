bd050f27b1c6b09f2dd30f6d38bc2f68f775b2ab
Joe Ferner <joe@fernsroth.com>
2014-07-09 09:51:21 -0400

    finish upgrade of mods

--- hardware/save-to-kicad-library.sh
+++ hardware/save-to-kicad-library.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-kicad-split --yes -i stm32-network-rs232.mod -o ./kicad-library/mods/
+cp mods/* kicad-library/mods/
 kicad-split --yes -i stm32-network-rs232.lib -o ./kicad-library/libs/
--- hardware/update-from-kicad-library.sh
+++ hardware/update-from-kicad-library.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-kicad-update -i stm32-network-rs232.mod.list -o stm32-network-rs232.mod --basedir ./kicad-library
+echo "Sync'ing mods"
+for f in mods/*
+do
+  FILENAME=$(basename $f)
+  KICADLIB_FILENAME=./kicad-library/mods/${FILENAME}
+  LOCAL_FILENAME=./mods/${FILENAME}
+  if [ -f ${KICADLIB_FILENAME} ]; then
+    cp ${KICADLIB_FILENAME} ${LOCAL_FILENAME}
+  else
+    echo "Could not find ${KICADLIB_FILENAME}"
+  fi
+done
+
 kicad-update -i stm32-network-rs232.lib.list -o stm32-network-rs232.lib --basedir ./kicad-library
