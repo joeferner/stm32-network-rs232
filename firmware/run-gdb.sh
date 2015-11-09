021a3a2804d139017db0c33a1e069bf0b4aa1bfd
Joe Ferner <joe@fernsroth.com>
2015-11-09 12:29:01 -0500

    remove old firmware

--- firmware/run-gdb.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-PROJECT_NAME=$(sed -n -e 's/PROJECT(\(.*\))/\1/p' CMakeLists.txt)
-echo "Running GDB ${PROJECT_NAME}"
-
-arm-none-eabi-gdb -tui -ex "target extended-remote localhost:4242" build/${PROJECT_NAME}.elf
